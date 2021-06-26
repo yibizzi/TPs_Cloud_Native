74c3996a2f169edf04a100ab901a299c859efc9b
yibizzi <ibizzi.younes@ine.inpt.ma>
2021-06-26 14:40:35 +0100

    TP3 Automatisation

---
+++ TP3 Automatisation/serverless-naruto-app/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyDdm0h6EtwMcgIkYGNjaWzJ_jluxSzz6_M',
+    authDomain: 'cours-tp.firebaseapp.com',
+    projectId: 'cours-tp',
+    storageBucket: 'cours-tp.appspot.com',
+    messagingSenderId: '954543312031',
+    appId: '1:954543312031:web:c36d681fbbc7c46fd166aa'
+  }
+};
